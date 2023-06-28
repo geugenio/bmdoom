c532bf75bff35cd233d179e4e27226334cc3fa2d
geugenio <geugnio12@gmail.com>
2023-06-28 11:20:33 -0300

    aula 28/08/2023

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Cabecalho from "./componentes/Cabecalho"
+import Rodape from './componentes/Rodape';
+/*
+Arquivo APP JSX
 
+Componente funcional = Ele é uma função!
+*/
 function App() {
+
+  /*Todo componente deve retornar um html entre paretne*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // O react substitui className por class pq class é palavra reservada do JS.
+    <>
+    <Cabecalho />
+    <Rodape />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+//  Arquivo principal. Serve para iniciar a aplicação
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import logo from "logo.svg; a "logo" é como o arquivo será chamado onde está sendo importado!
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*Renderiza a aplicação no navegador*/
+
 root.render(
   <React.StrictMode>
     <App />
