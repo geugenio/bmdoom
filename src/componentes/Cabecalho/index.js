0cb32239e167f2ba87d25eeafb318e8a28066fd2
Gabriel Eugenio <geugnio12@gmail.com>
2023-07-26 21:58:37 -0300

    adicionado menu como componente

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,24 +1,12 @@
 import React from 'react'
 import styles from "./Cabecalho.module.css"
+import Menu from "../Menu/";
 export default function Cabecalho(props) {
   return ( 
     <div className={styles.cabecalho}>
       <img src = {props.logo} alt=""></img>
       <p className={styles.titulo} >{props.titulo}</p>
-      <nav>
-        <ul>
-          <li>
-            <a href="">HOME</a>
-          </li>
-          <li>
-            <a href="">SUAS COMPRAS</a>
-          </li>
-          <li>
-            <a href="">SOBRE</a>
-          </li>
-        </ul>
-      </nav>
-      {/* <p className={`${styles.titulo} ${styles.subtitulo}`} >{props.subtitulo}</p> */}
+      <Menu/>
     </div>
   )
 }
---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./Menu.module.css"
+
+export default function Menu() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><a href="##">HOME</a></li>
+          <li><a href="##">SEUS FAVORITOS</a></li>
+          <li><a href="##">SOBRE NÓS</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
